ecf2da2608d54afd23ca768ae28d3e47fca73e74
IvanYarochkin <Ivan.Yarochkin97@gmail.com>
2017-12-17 15:15:48 +0300

    Remove a main method in an UnMarshal class

---
+++ src/main/java/com/yarachkin/xml/exception/XmlParserException.java
@@ -0,0 +1,18 @@
+package com.yarachkin.xml.exception;
+
+public class XmlParserException extends Exception {
+    public XmlParserException() {
+    }
+
+    public XmlParserException(String message) {
+        super(message);
+    }
+
+    public XmlParserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public XmlParserException(Throwable cause) {
+        super(cause);
+    }
+}
--- src/main/java/com/yarachkin/xml/marshal/FlowerIUnMarshalMain.java
+++ src/main/java/com/yarachkin/xml/marshal/FlowerIUnMarshalMain.java
@@ -1,6 +1,7 @@
 package com.yarachkin.xml.marshal;
 
 import com.yarachkin.xml.entity.Flowers;
+import com.yarachkin.xml.exception.XmlParserException;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
@@ -12,19 +13,20 @@ import javax.xml.validation.SchemaFactory;
 import java.io.File;
 
 public class FlowerIUnMarshalMain {
-    public static void main(String[] args) {
+
+    public Flowers unMarshal(String xmlPath, String xsdPath) throws XmlParserException {
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(Flowers.class);
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            String schemaName = "src/main/resources/flowers.xsd";
+            String schemaName = xsdPath;
             SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
             File schemaLocation = new File(schemaName);
             Schema schema = factory.newSchema(schemaLocation);
             unmarshaller.setSchema(schema);
-            Flowers flowers = (Flowers) unmarshaller.unmarshal(new File("src/main/resources/flowers.xml"));
-            System.out.println(flowers);
+            Flowers flowers = (Flowers) unmarshaller.unmarshal(new File(xmlPath));
+            return flowers;
         } catch (JAXBException | SAXException e) {
-            e.printStackTrace();
+            throw new XmlParserException(e);
         }
     }
 }
